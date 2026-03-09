bb1168f712afe4f0a0ea5b9624568b5880e79b79
atharvchougule0707 <atharvchougule0707@gmail.com>
2026-03-09 14:51:50 +0530

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,43 +1,67 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int queue[5];
-int front = -1, rear = -1;
+int stack[100];
+int top=-1;
+int size;
 
-void enqueue(int x) {
-    if (rear == 4)
-        cout << "Queue Full\n";
-    else {
-        if (front == -1) front = 0;
-        queue[++rear] = x;
-    }
-}
+int main()
+{
+    int ch,x;
 
-void dequeue() {
-    if (front == -1 || front > rear)
-        cout << "Queue Empty\n";
-    else
-        front++;
-}
+    cout<<"Enter size of stack: ";
+    cin>>size;
 
-void display() {
-    for (int i = front; i <= rear; i++)
-        cout << queue[i] << " ";
-    cout << endl;
-}
+    do{
+        cout<<"\n1.Push\n2.Pop\n3.Top\n4.isEmpty\n5.isFull\n6.Exit\n";
+        cout<<"Enter choice: ";
+        cin>>ch;
 
-int main() {
-    enqueue(10);
-    enqueue(20);
-    enqueue(30);
-    enqueue(40);
-    enqueue(50);
-    enqueue(60);
-    enqueue(70);
-    display();
+        if(ch==1)
+        {
+            if(top==size-1)
+                cout<<"Stack Full\n";
+            else
+            {
+                cout<<"Enter element: ";
+                cin>>x;
+                top++;
+                stack[top]=x;
+            }
+        }
+        else if(ch==2)
+        {
+            if(top==-1)
+                cout<<"Stack Empty\n";
+            else
+            {
+                cout<<"Deleted element "<<stack[top]<<endl;
+                top--;
+            }
+        }
+        else if(ch==3)
+        {
+            if(top==-1)
+                cout<<"Stack Empty\n";
+            else
+                cout<<"Top element "<<stack[top]<<endl;
+        }
+        else if(ch==4)
+        {
+            if(top==-1)
+                cout<<"Stack is Empty\n";
+            else
+                cout<<"Stack is Not Empty\n";
+        }
+        else if(ch==5)
+        {
+            if(top==size-1)
+                cout<<"Stack is Full\n";
+            else
+                cout<<"Stack is Not Full\n";
+        }
 
-    dequeue();
-    display();
+    }while(ch!=6);
 
     return 0;
 }
